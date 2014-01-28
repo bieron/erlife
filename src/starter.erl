8166101b9d0375acb4d58f96a02da23d61fe09b8
sidewalker <sidewalker@sidewalker-R580-R590.(none)>
2014-01-28 03:44:02 +0100

    starter v4

--- src/starter.erl
+++ src/starter.erl
@@ -1,17 +1,24 @@
 -module(starter).
--export([start/1, next/1, next/0, stop/0]).
+-export([start/1, next/1, next/0, stop/0, read_and_start/1]).
 
 start(BoardSize) ->
+	init(),
+	lifeio:testWrite(BoardSize),
+	master_bula:start(),
+	c:nl(slave_bula).
+
+read_and_start(FileName) ->
+	init(). % tu zmien . na ,
+	%master_bula:start(FileName).
+
+init() ->
 	c:l(master_bula),
 	c:l(slave_bula),
 	c:nl(lifeio),
 	c:nl(master_bula),
 	c:nl(slave_bula),
 	c:nl(board_utils_bula),
-	c:nl(benchmark),
-	lifeio:testWrite(BoardSize),
-	master_bula:start(),
-	c:nl(slave_bula).
+	c:nl(benchmark).
 
 stop() ->
 	master_bula:stop().
